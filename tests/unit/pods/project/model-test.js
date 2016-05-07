e23b6befb2aa529bc69cb29272f297ad53fd427b
Pau Compte <pausandalio@gmail.com>
2016-05-07 19:04:35 +0200

    All tests green again

--- tests/unit/pods/project/model-test.js
+++ tests/unit/pods/project/model-test.js
@@ -2,7 +2,9 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('project', 'Unit | Model | project', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    "model:goal"
+  ]
 });
 
 test('it exists', function(assert) {
